46ffcd87242c974c1cd8fbd23f7434ac941ac284
Tatsiana_Kryshtofik <tatiana-kri@tut.by>
2022-02-09 21:02:31 +0300

    refreshed urls

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('auth/register/', RegistrationAPIView.as_view()),
     path('auth/login/', LoginAPIView.as_view()),
     path('auth/update/', UserUpdateAPIView.as_view()),
+    path('userinfo/', AddUserInfoAPIView.as_view({'post': 'create', 'get': 'list'})),
+    path('userinfo/<int:pk>/', AddUserInfoAPIView.as_view({'put': 'update', 'get': 'retrieve'})),
 
 ]
