464d9793bc40c9f8e39f56802ba82139e4b94998
Tatsiana_Kryshtofik <tatiana-kri@tut.by>
2022-02-09 19:25:48 +0300

    fixed serializers

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,6 +1,4 @@
-from django.contrib.auth import authenticate
-
-from .models import User, Post, UserInfo, Comment, Image, PostRating
+from .models import *
 from rest_framework import serializers
 
 
@@ -9,18 +7,26 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'date_joined']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('first_name', 'last_name',
+                  'username', 'email', 'phone_number',
+                  'password', 'date_joined')
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
 
     def create(self, validated_data):
 
-        user = User(email=self.validated_data['email'], username=self.validated_data['username'])
+        user = User(email=self.validated_data['email'], username=self.validated_data['username'],
+                    first_name=self.validated_data['first_name'],
+                    last_name=self.validated_data['last_name'],
+                    phone_number=self.validated_data['phone_number'])
         user.set_password(self.validated_data['password'])
         user.save()
         return user
 
 
 class LoginSerializer(serializers.Serializer):
+
     email = serializers.EmailField()
     username = serializers.CharField(max_length=50, write_only=True)
     password = serializers.CharField(max_length=50, write_only=True)
@@ -34,7 +40,7 @@ class LoginSerializer(serializers.Serializer):
             raise serializers.ValidationError(
                 'An email address is required to log in.'
             )
-        if password is None:
+        if username is None:
             raise serializers.ValidationError(
                 'An username is required to log in.'
             )
@@ -42,74 +48,20 @@ class LoginSerializer(serializers.Serializer):
             raise serializers.ValidationError(
                 'A password is required to log in.'
             )
-        user = authenticate(username=username, email=email,
-                            password=password)
-        if user is None:
-            raise serializers.ValidationError(
-                'A user with this email and password was not found.'
-            )
-        if not user.is_active:
-            raise serializers.ValidationError(
-                'This user has been deactivated.'
-            )
-        return {
-            'email': user.email,
-            'username': user.username
-        }
+        return data
 
 
-class UserSerializer(serializers.ModelSerializer):
-
-    password = serializers.CharField(
-        max_length=50,
-        min_length=6,
-        write_only=True
-    )
+class UserUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('email', 'username', 'password',)
+        fields = ('first_name', 'last_name', 'phone_number')
 
     def update(self, instance, validated_data):
-
-        password = validated_data.pop('password', None)
-        for key, value in validated_data.items():
+        for (key, value) in validated_data.items():
             setattr(instance, key, value)
-        if password is not None:
-            instance.set_password(password)
         instance.save()
         return instance
 
-class PostSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Post
-        fields = ['id', 'title', 'category', 'subcategory', 'description', 'user']
 
 
-class UserInfoSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = UserInfo
-        fields = ['country', 'city', 'user']
-
-
-class CommentSerializer(serializers.ModelSerializer):
-
-    class Meta(object):
-        model = Comment
-        fields = ['user', 'post', 'description']
-
-
-class ImageSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Image
-        fields = ['image url', 'created_at']
-
-
-class PostRatingSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = PostRating
-        fields = ['user', 'post', 'value']
